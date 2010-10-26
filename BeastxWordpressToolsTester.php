849da9ede84d2535981ce516d1a69ab9f02925ef
Leandro Asrilevich <beastx@gmail.com>
2010-10-26 02:55:20 -0300

    added initial tester class code

--- BeastxWordpressToolsTester.php
+++ BeastxWordpressToolsTester.php
@@ -30,14 +30,119 @@ Text Domain: BeastxWPProjects
 if (!defined('ABSPATH')) die("Aren't you supposed to come here via WP-Admin?");
 
 require_once 'BeastxWordpressTools/includeBeastxWordpressTools.php';
+require_once 'ProjectPostType.php';
 
 if (!class_exists('BeastxWordpressToolsTester')) {
 
 Class BeastxWordpressToolsTester extends BeastxPlugin {
 
     public function __construct() {
+        parent::__construct();
         
+        $this->testRegisterPluginLinks();
+        $this->testRegisterDashboardWidget();
+        $this->testRegisterCustomHelp();
+        $this->testRegisterFavoriteOption();
+        $this->testRegisterBuiltInAssets();
+        $this->testRegisterAssets();
+        $this->testRegisterExternalAssets();
+        $this->testRegisterInlineAssets();
+        
+        $this->postType = new ProjectPostType();
+        debug($this->postType);
     }
+    
+    function testRegisterBuiltInAssets() {
+        $this->addRowEditorAssets(array(array('section' => 'posts', 'subSection' => 'addnew')));
+        $this->addRowJqueryUIAssets(array(array('section' => 'posts', 'subSection' => 'addnew')));
+    }
+    
+    function testRegisterAssets() {
+        $this->registerStyle(
+            'adminpage',
+            'adminpage.css',
+            array(array('section' => 'posts', 'subSection' => 'addnew'))
+        );
+        
+        $this->registerScript(
+            'RowEditor',
+            'RowEditor.js',
+            array(array('section' => 'settings', 'subSection' => 'reading')),
+            array(),
+            '2.1',
+            true
+        );
+    }
+    
+    function testRegisterExternalAssets() {
+        $this->registerExternalStyle(
+            'ikariam',
+            'http://s2.ar.ikariam.com/skin/ik_common_0.4.0.css',
+            array(array('section' => 'settings', 'subSection' => 'reading'))
+        );
+        
+        $this->registerExternalScript(
+            'ikariam',
+            'http://s2.ar.ikariam.com/js/complete-0.4.0.js',
+            array(array('section' => 'settings', 'subSection' => 'reading'))
+        );
+    }
+    
+    function testRegisterInlineAssets() {
+        $this->registerInlineStyle(
+            "body #site-title { color: red !important; }",
+            array(array('section' => 'settings', 'subSection' => 'discussion'))
+        );
+        $this->registerInlineScript(
+            "console.log('pepe')",
+            array(array('section' => 'settings', 'subSection' => 'discussion')),
+            true
+        );
+    }
+    
+    function testRegisterPluginLinks() {
+        $this->registerPluginActionLink('PEPE1', 'http://www.pepe1.com');
+        $this->registerPluginMetaLink('PEPE2', 'http://www.pepe2.com');
+    }
+    
+    function testRegisterDashboardWidget() {
+        $this->registerDashboardWidget('dashboardWidgetTest', 'Dashboard Widget Test', array(&$this, 'dashboardWidget'));
+    }
+    
+    function testRegisterCustomHelp() {
+        $this->registerCustomHelp(
+            array(
+                'section' => 'settings',
+                'subSection' => 'general'
+            ),
+            array(&$this, 'optionsCustomHelp'),
+            true
+        );
+    }
+    
+    function testRegisterFavoriteOption() {
+        $this->removeFavoriteOption('edit.php?post_status=draft');
+        $this->addFavoriteOption('http://www.pepe3.com', 'Pepe3');
+    }
+    
+    public function optionsCustomHelp() {
+        echo '<h1>Custom Help Test</h1>';
+        echo '<p>Esto es un test!</p>';
+    }
+    
+    public function dashboardWidget() {
+        echo '<h1>Custom Dashboard Widget Test</h1>';
+        echo '<p>Esto es un test!</p>';
+    }
+    
+    public function onInit() { debug('BeastxWordpressToolsTester::onInit'); }
+    public function onAdminInit() { debug('BeastxWordpressToolsTester::onAdminInit'); }
+    public function onPluginLoad() { debug('BeastxWordpressToolsTester::onPluginLoad'); }
+    public function onPluginActivate() { debug('BeastxWordpressToolsTester::onPluginActivate'); }
+    public function onPluginDeactivate() { debug('BeastxWordpressToolsTester::onPluginDeactivate'); }
+    public function onSavePost() { debug('BeastxWordpressToolsTester::onSavePost'); }
+    public function onGetContent() { debug('BeastxWordpressToolsTester::onGetContent'); }
+    public function onGetPosts() { debug('BeastxWordpressToolsTester::onGetPosts'); }
     
 }
 
